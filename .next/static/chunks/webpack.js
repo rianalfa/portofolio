4da0ff62c244d7ad7ec04279dd771f44087d8869
iclc8212@iclasseducation.id <221810560@stis.ac.id>
2022-10-23 13:27:03 +0700

    fixing scripts name in package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62d3e2c504d2da11"; }
+/******/ 		__webpack_require__.h = function() { return "c7b1edf5d79e9a24"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
